13f3435104215c631d62a635dbaf745efe5e80e8
Wisnu Box <hwisnu222@gmail.com>
2023-08-23 11:26:48 +0700

    feat: add profile and wishlist

--- src/api/wishlist/controllers/wishlist.js
+++ src/api/wishlist/controllers/wishlist.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /**
  * wishlist controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::wishlist.wishlist');
+module.exports = createCoreController("api::wishlist.wishlist");
